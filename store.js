bdd963d512d3802bb2c541e49198330027ddeabe
karanMalhii <karanmalhii1313@gmail.com>
2024-02-26 17:04:20 +0530

    first commit

---
+++ store.js
@@ -0,0 +1,19 @@
+const store = {
+  todos: [
+    {
+      id: "1",
+      title: "Complete Task A",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Read Book",
+      completed: true,
+    },
+    {
+      id: "1",
+      title: "Write Code",
+      completed: false,
+    },
+  ],
+};
